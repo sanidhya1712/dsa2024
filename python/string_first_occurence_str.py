fa7b443bcf7eab6897b93377fe0bd9fd85b2d040
sanidhyatruminds <sanidhya.daeeyya@truminds.com>
2025-01-02 22:21:41 +0530

    new year starting

---
+++ python/string_first_occurence_str.py
@@ -0,0 +1,14 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+
+        if len(haystack) < len(needle):
+            return -1
+
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+
+        return -1 
+
+s= Solution()
+print(s.strStr("sadbutsad", "sad"))
