84458573b4ae64440057668e09410c666e7f71f7
Sanidhya1712 <sanidhyaranger@gmail.com>
2025-06-06 17:52:22 +0530

    comments

--- striver_AtoZdsa/step3Arrays/lec2/longest_consecutive_subSequence.py
+++ striver_AtoZdsa/step3Arrays/lec2/longest_consecutive_subSequence.py
@@ -1,4 +1,3 @@
-
 def longestConsecutiveSubsequence(arr):
     n = len(arr)
     if n == 0:
@@ -17,4 +16,16 @@ def longestConsecutiveSubsequence(arr):
             lastSmaller = arr[i]
         maxSeq = max(seq, maxSeq)
     return maxSeq
-print(longestConsecutiveSubsequence([7, 1, 1, 5, 3, 6, 4]))
+#Optimal with set, will complete this with set later
+def longestConsecutiveSubsequence(arr):
+    arr.sort()
+    seq = 1
+    print(arr)
+    for i in range(1,len(arr)):
+        if arr[i-1]== arr[i]-1:
+            seq +=1
+        else:
+            seq = 1
+        maxSeq = max(seq, maxSeq)
+    return maxSeq
+print(longestConsecutiveSubsequence([7, 1, 5, 3, 6, 4]))
